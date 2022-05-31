cdcf3db921d5e0e07b1494abe84322abbf0564ad
Santiago Ramírez <35697573+santrami@users.noreply.github.com>
2022-05-30 19:43:24 -0500

    terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import Characters from './components/Characters'
+import SearchCharacterForm from './components/SearchCharacter'
+import ButtonNavigation from './components/ButtonNavigation'
+
+import './App.css'
+import { DataProvider } from './context/DataContext'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataProvider>
+        <SearchCharacterForm/>   
+        <ButtonNavigation />     
+        <Characters/>     
+        <ButtonNavigation />
+      </DataProvider>
     </div>
   );
 }
